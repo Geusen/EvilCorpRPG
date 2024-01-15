ce55d98245b99a3a23595fd4f4910f3b4d7a1eef
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-15 16:26:19 +0000

    subHp and roomNR function tested

--- src/EvilCorpRPG.js
+++ src/EvilCorpRPG.js
@@ -4,12 +4,17 @@ const prompt = require('prompt-sync')({sigint: true});
 
 /***** Raumzähler *****/
 //      roomNR 
+//output(roomNr(100,20)); //function subHp test
 
 
+function roomNr(nowRoom, choosenRoom) {
+    return nowRoom + choosenRoom;
+}
 /***** HP Rechner *****/
 //      trueHp
 //      subtractHp
-output(subHp(100,20));
+//output(subHp(100,20)); //function subHp test
+
 function subHp(trueHp, damage) {
     return trueHp - damage;
 }
@@ -17,6 +22,13 @@ function subHp(trueHp, damage) {
 
 
 /***** Mana Rechner *****/
+
+//output(submana(500,20)); //function subHp test
+
+function submana(trueMana, subMana) {
+    return trueMana - subMana;
+}
+
 /*****  *****/
 /*****  *****/
 // console.log("hi");  // Zugriffstest

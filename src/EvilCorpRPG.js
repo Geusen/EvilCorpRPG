a2bffd69226c943ff159c64fd3688eef5978ec8e
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-15 16:09:45 +0000

    subHp function tested

--- src/EvilCorpRPG.js
+++ src/EvilCorpRPG.js
@@ -1,49 +1,42 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+
+
 /***** Raumzähler *****/
 //      roomNR 
 
 
 /***** HP Rechner *****/
 //      trueHp
-//      
-/***** Mana Rechner *****/
-/*****  *****/
-/*****  *****/
-console.log("hi");
-/*
-
-const prompt = require('prompt-sync')({sigint: true});
-
-let starthP = parseFloat(100);
-let raumNR = parseFloat(1);
-
-if (raumNR = 1) {
-    let firstName = prompt("Vorname?: ");
-let familyName = prompt("Name?: ");
-
-
-console.log("Hallo, " + firstName + " " + familyName +"!" + "dein Abenteuer beginnt hier." + "deine Lebensenergie liegt bei: " + starthP + " Du befindest dich in Raum " + raumNR
-+ "gehst du nach rechts (2) oder links (3)?");
-
-raumNR =  parseFloat(prompt("Raum?: "));
+//      subtractHp
+output(subHp(100,20));
+function subHp(trueHp, damage) {
+    return trueHp - damage;
 }
 
 
-if (raumNR <= 2) {
-   
-    hp = 20;
 
-    starthp = -hp
-    console.log("raum 2!" + "deine Lebensenergie liegt bei: " + starthP );
-
-    }
+/***** Mana Rechner *****/
+/*****  *****/
+/*****  *****/
+// console.log("hi");  // Zugriffstest
 
 
+/***** add *****/
 
-if (raumNR >= 3) {
-    
-    hp = 10
+/***** subtract *****/
 
-    starthp = -hp
-    console.log("raum 3!" + "deine Lebensenergie liegt bei: " + starthP );
-    
+function sutract(a,b) {
+    return a - b;
+}
+
+
+
+
+// module: output | test:
+
+ //output("hello");
+ //output(2);
+function output(outputData) {
+	console.log(outputData);
 }
